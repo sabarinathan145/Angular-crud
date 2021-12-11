b4c4be2a0f199404d05fc8f251026118a0ef1c99
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-12-11 19:21:21 +0530

    delete nodejs directory

--- nodejsproject/api/routes/product.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = express.Router();
-const products = require('../models/products');
-
-module.exports = router;
