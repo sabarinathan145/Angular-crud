cd7ee79e6f530a4f2e9beb870faa3e3da4a69fe8
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-12-11 19:31:12 +0530

    Add files via upload

---
+++ src/app/_hosts/hosts.ts
@@ -0,0 +1,12 @@
+var host = window.location.hostname, protocol = window.location.protocol;
+
+var Backend='';
+
+ if(host == 'localhost'){
+ 	Backend = "http://"+host+":3000/";
+ }
+  else{
+    Backend = "http://"+host+":3000/";
+ }
+ console.log(Backend);
+export var BackendHost = Backend;
